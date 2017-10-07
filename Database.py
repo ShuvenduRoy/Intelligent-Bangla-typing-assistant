4b380404b3813ef18ae85112acaacd1f46466b58
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-07 15:56:51 +0600

    insrting into database

--- Database.py
+++ Database.py
@@ -10,16 +10,31 @@ class database_handler():
         try:
             con = lite.connect('test.db')
 
-            cur = con.cursor()
-            cur.execute('SELECT SQLITE_VERSION()')
-
-            data = cur.fetchone()
-
-            print("SQLite version: %s" % data)
+            # Test connection
+            # cur = con.cursor()
+            # cur.execute('SELECT SQLITE_VERSION()')
+            #
+            # data = cur.fetchone()
+            #
+            # print("SQLite version: %s" % data)
+            return con
 
         except lite.Error as e:
             print("Error %s:" % e.args[0])
             sys.exit(1)
 
 
-database_handler.connect()
+    @staticmethod
+    def insert_sentence(string):
+        con = database_handler.connect()
+        with con:
+            cur = con.cursor()
+            cur.execute("create table if not exists history(data TEXT)")
+
+            execution_string = "insert into history VALUES ('%s')" % string
+            cur.execute(execution_string)
+
+
+
+conn = database_handler.connect()
+database_handler.insert_sentence("this is a test")
