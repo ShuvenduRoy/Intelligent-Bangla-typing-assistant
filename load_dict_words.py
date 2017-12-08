40ace96bedccb2a64f8f8f8b7540cf87464d9c8c
Shuvendu Bikash <bikash11roy@gmail.com>
2017-12-08 22:53:11 +0600

    loading data of words from dictionary

---
+++ load_dict_words.py
@@ -0,0 +1,46 @@
+def load_english_word():
+    file = open('data/words.txt')
+
+    global eng_words
+    eng_words = []
+    for line in file:
+        eng_words.append(line)
+
+
+def english_word_search(start):
+    if 'eng_words' not in globals():
+        load_english_word()
+
+    words_with_start = []
+    for word in eng_words:
+        if word.startswith(start):
+            words_with_start.append(word[:-1])
+
+            if len(words_with_start) == 5:
+                return words_with_start
+
+
+def load_bangla_word():
+    import io
+    file = io.open("data/Bangla_word.txt", mode="r", encoding="utf-8")
+
+    global bangla_words
+    bangla_words = []
+    for line in file:
+        bangla_words.append(line)
+
+
+def bangla_word_search(start):
+    if 'bangla_words' not in globals():
+        load_bangla_word()
+
+    words_with_start = []
+    for word in bangla_words:
+        if word.startswith(start):
+            words_with_start.append(word[:-1])
+
+            if len(words_with_start) == 5:
+                return words_with_start
+
+
+print(bangla_word_search('যম'))
