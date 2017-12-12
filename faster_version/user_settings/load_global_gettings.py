bb8a6cf19f2b5f8355eee021e474a4249868ad22
Shuvendu Bikash <bikash11roy@gmail.com>
2017-12-13 01:18:50 +0600

    Control panel added

--- faster_version/user_settings/load_global_gettings.py
+++ faster_version/user_settings/load_global_gettings.py
@@ -1,4 +1,4 @@
-with open('user_settings/global_settings', 'r') as f:
+with open('user_settings/global_settings.txt', 'r') as f:
     language_settings = f.readline()
     global enabled_language
     enabled_language = 'bangla' if language_settings.split(' ')[1].startswith('bangla') else 'english'
